292806318fc401fa87be466363077c59a0edc175
Joseph Ware <ware.josephb@gmail.com>
2018-08-22 22:59:43 +0100

    Update Writetext.py

--- Writetext.py
+++ Writetext.py
@@ -1,3 +1,6 @@
+''' This code wrote an HTML page for each observation I was studying, which then were linked to from a master/home page.'''
+
+
 from os import listdir
 from astropy.io import fits
 
@@ -20,3 +23,12 @@ for K in listdir('/export/data/josephw/Data/'):
     for Q in i:
         L.write(Q+'\n')
     L.close()
+
+    
+    # For observation K of object R on date P make a page showing:
+    # E = 'trace' of image in timing mode
+    # J = 'source area' of image (a ~40 pixel wide strip shown by a coloured bar under the image, each used the same region)'
+    # F = Light curve of observation
+    # G = Lomb-Scargle periodogram of the data
+    
+    # This is an early enough version that the Fast-Fourier Transform was not included, but I also had that on the final version
